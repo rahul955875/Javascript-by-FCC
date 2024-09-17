78e96b7eb1ba65c7e34d7940074405605db8211d
rahul <nizarerahul7@gmail.com>
2024-09-17 22:12:47 +0530

    start

--- 05_formValidationUsingcounter/script.js
+++ 05_formValidationUsingcounter/script.js
@@ -1,1 +1,11 @@
-console.log("r");
+const calorieCounter = document.getElementById('calorie-counter');
+const budgetNumberInput = document.getElementById('budget');
+const entryDropdown = document.getElementById('entry-dropdown');
+const addEntryButton = document.getElementById('add-entry');
+const clearButton = document.getElementById('clear');
+const output = document.getElementById('output');
+let isError = false;
+
+function cleanInputString(str) {
+  const regex = /\+-/;
+}
